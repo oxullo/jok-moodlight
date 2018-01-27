f7dcca5c4872368387a95f1683b3a66ebb0dd7d0
OXullo Intersecans <x@brainrapers.org>
2018-01-27 22:21:41 +0100

    Prepare drops for gravity rules

--- src/rain.cpp
+++ src/rain.cpp
@@ -2,30 +2,89 @@
 #include <string.h>
 
 #include "leds.h"
+#include "LSM6.h"
 
-static int8_t hotspots[kMatrixWidth];
+#define MAX_DROPS       12
+
+
+class Drop {
+public:
+    Drop();
+    bool is_alive();
+    void start();
+    void step();
+    uint8_t get_x();
+    uint8_t get_y();
+
+
+private:
+    bool alive;
+    float x;
+    float y;
+};
+
+Drop::Drop() :
+        alive(false),
+        x(0),
+        y(0)
+{
+}
+
+bool Drop::is_alive()
+{
+    return alive;
+}
+
+void Drop::start()
+{
+    x = random8(kMatrixWidth);
+    y = 0;
+    alive = true;
+}
+
+void Drop::step()
+{
+    y += 0.5;
+
+    if (y > kMatrixHeight - 1) {
+        alive = false;
+    }
+}
+
+uint8_t Drop::get_x()
+{
+    return (uint8_t)x;
+}
+
+uint8_t Drop::get_y()
+{
+    return (uint8_t)y;
+}
+
+
+static Drop drops[MAX_DROPS];
 
 void rain_init()
 {
-    memset(hotspots, -1, sizeof(hotspots));
 }
 
 void rain_render()
 {
     fadeToBlackBy(leds, NUM_LEDS, 20);
 
-    for (uint8_t i = 0 ; i < kMatrixWidth ; ++i) {
-        if (hotspots[i] != -1) {
-            leds[XY(i, hotspots[i])] = CRGB::Blue;
-            ++hotspots[i];
-
-            if (hotspots[i] == kMatrixHeight) {
-                hotspots[i] = -1;
-            }
+    for (uint8_t i = 0 ; i < MAX_DROPS ; ++i) {
+        if (drops[i].is_alive()) {
+            leds[XY(drops[i].get_x(), drops[i].get_y())] = CRGB::Blue;
+            drops[i].step();
         }
     }
 
     if (random8(100) > 90) {
-        hotspots[random8(kMatrixWidth)] = 0;
+        for (uint8_t i = 0 ; i < MAX_DROPS ; ++i) {
+            if (!drops[i].is_alive()) {
+                drops[i].start();
+                break;
+            }
+        }
     }
 }
